eeb2acbcf8a7a486b07133f516bdd8d45141c709
Diego Puentes <djsix2001@yahoo.com>
2025-02-06 05:41:15 -0500

    .

--- logic/logic_sup.py
+++ logic/logic_sup.py
@@ -1,36 +1,62 @@
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+from logic.error import Len
+
 
 class Sup:
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
-    def plot_homog(self):
+    def plot_homog(self) -> None:
         pass
 
-    def plot_no_homog(self):
+    def plot_no_homog(self) -> None:
         pass
 
-    def get_solution(self, array, cond_iniciales, HOMOG=True):
-        x = sp.Symbol("x")
+    def get_solution(
+        self,
+        array: list[tuple[int | float]],
+        cond_iniciales: list[int | float],
+        HOMOG=True,
+    ) -> tuple[str, callable]:
+
+        x = sp.symbols("x")
         y = sp.Function("y")
         C_symbols = sp.symbols(f"C1:{len(cond_iniciales) + 1}")
         if HOMOG:
+
             ode_homogenea = sum(coef * y(x).diff(x, grado) for coef, grado in array)
             sol_homogenea = sp.dsolve(sp.Eq(ode_homogenea, 0), y(x))
+
             expr = sol_homogenea.rhs
             for i, cond in enumerate(cond_iniciales):
                 expr = expr.subs(C_symbols[i], cond)
-            expr_eval = expr.evalf(3)
+
             f_numeric = sp.lambdify(x, expr, "numpy")
-            return sp.latex(expr_eval), f_numeric
+
+            return (
+                sp.latex(sol_homogenea),
+                f_numeric,
+            )  # f_numeric, es la funcion resultado numerica ya, le puedes hacer f_numeric(numeros)
+
         rhs = array[-1]
         ode_no_homogenea = sum(coef * y(x).diff(x, grado) for coef, grado in array[:-1])
+
         sol_no_homogenea = sp.dsolve(sp.Eq(ode_no_homogenea, rhs), y(x))
         expr = sol_no_homogenea.rhs
         for i, cond in enumerate(cond_iniciales):
             expr = expr.subs(C_symbols[i], cond)
-        expr_eval = expr.evalf(3)
         f_numeric = sp.lambdify(x, expr, "numpy")
-        return sp.latex(expr_eval), f_numeric
+
+        return (
+            sp.latex(sol_no_homogenea),
+            f_numeric,
+        )  # f_numeric, es la funcion resultado numerica ya, le puedes hacer f_numeric(numeros)
+
+
+# ejemplo
+sup = Sup()
+# sup.get_solution([(2, 2), (1, 1)], [1, 2])
+# tienes que limitar las opciones de las no homogenias, xq necesitas pasarlas simbolicamete con sympy
+# sup.get_solution([(2, 2), (1, 1), sp.sin(sp.symbols("x"))], [1, 2], False)
