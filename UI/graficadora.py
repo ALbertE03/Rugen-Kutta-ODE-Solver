0a688b29af2223390fd6bb910fb9ce295d99379c
ALbertEO3 <marichalalberto292@gmail.com>
2025-02-07 09:53:12 -0500

    session state

--- UI/graficadora.py
+++ UI/graficadora.py
@@ -14,22 +14,23 @@ st.subheader("Graficadora")
 input_col, plot_col = st.columns([1, 2])
 
 with input_col:
-    equation_str = st.text_input(
-        "Ingresa la ecuación diferencial (ej. 'sen(x-y)'):", ""
-    )
+    with st.container(border=True):
+        equation_str = st.text_input(
+            "Ingresa la ecuación diferencial (ej. 'sen(x-y)'):", ""
+        )
 
-    col1, col2 = st.columns(2)
-    with col1:
-        x0 = st.number_input("Valor inicial de x (x0):", value=0.0)
-    with col2:
-        y0 = st.number_input("Valor inicial de y (y0):", value=1.0)
+        col1, col2 = st.columns(2)
+        with col1:
+            x0 = st.number_input("Valor inicial de x (x0):", value=0.0)
+        with col2:
+            y0 = st.number_input("Valor inicial de y (y0):", value=1.0)
 
-    h = st.number_input("Paso de integración (h):", value=0.1)
-    xf = st.number_input("Valor final de x:", value=10.0)
-
-    col1, col2 = st.columns(2)
-    resolver_pressed = col1.button("Resolver Orden 4")
-    comparar_pressed = col2.button("Comparar Orden 3")
+        h = st.number_input("Paso de integración (h):", value=0.1)
+        xf = st.number_input("Valor final de x:", value=10.0)
+        isoclinas = st.toggle("isoclinas")
+        col1, col2 = st.columns(2)
+        resolver_pressed = col1.button("Resolver RK-4")
+        comparar_pressed = col2.button("Comparar RK-3")
 
 with plot_col:
     if resolver_pressed or comparar_pressed:
@@ -51,15 +52,16 @@ with plot_col:
                     {"x": X_iso, "y": Y_iso, "u": U_iso, "v": V_iso}
                 )
 
-                line_chart = (
-                    alt.Chart(line_data)
-                    .mark_line()
-                    .encode(
-                        x=alt.X("x:Q", scale=alt.Scale(domain=(x_min, x_max))),
-                        y=alt.Y("y:Q", scale=alt.Scale(domain=(y_min, y_max))),
+                if "line_chartrk_4" not in st.session_state:
+                    line_chart = (
+                        alt.Chart(line_data)
+                        .mark_line()
+                        .encode(
+                            x=alt.X("x:Q", scale=alt.Scale(domain=(x_min, x_max))),
+                            y=alt.Y("y:Q", scale=alt.Scale(domain=(y_min, y_max))),
+                        )
                     )
-                )
-
+                    session_state.line_chartrk_4 = line_chart
                 arrow_length = 0.2 * scale_factor
                 quiver_data["x2"] = quiver_data["x"] + quiver_data["u"] * arrow_length
                 quiver_data["y2"] = quiver_data["y"] + quiver_data["v"] * arrow_length
@@ -71,10 +73,14 @@ with plot_col:
                     .mark_line(color="red")
                     .encode(x="x:Q", y="y:Q", x2="x2:Q", y2="y2:Q")
                 )
-
-                combined_chart = alt.layer(line_chart, arrows).properties(
-                    width=600, height=400
-                )
+                if isoclinas:
+                    combined_chart = alt.layer(
+                        st.session_state.line_chartrk_4, arrows
+                    ).properties(width=600, height=400)
+                else:
+                    combined_chart = alt.layer(
+                        st.session_state.line_chartrk_4
+                    ).properties(width=600, height=400)
 
                 if comparar_pressed:
                     rk_solver_3 = RungeKutta(x0, y0, xf, h, equation_str)
@@ -84,19 +90,25 @@ with plot_col:
                     y_max = max(y_max, max(Y3))
 
                     line_data3 = pd.DataFrame({"x": X3, "y": Y3})
-                    line_chart3 = (
-                        alt.Chart(line_data3)
-                        .mark_line(color="green")
-                        .encode(
-                            x=alt.X("x:Q", scale=alt.Scale(domain=(x_min, x_max))),
-                            y=alt.Y("y:Q", scale=alt.Scale(domain=(y_min, y_max))),
+                    if "line_chartrk_3" not in st.session_state:
+                        line_chart3 = (
+                            alt.Chart(line_data3)
+                            .mark_line(color="green")
+                            .encode(
+                                x=alt.X("x:Q", scale=alt.Scale(domain=(x_min, x_max))),
+                                y=alt.Y("y:Q", scale=alt.Scale(domain=(y_min, y_max))),
+                            )
                         )
-                    )
+                        st.session_state.line_chartrk_3 = line_chart3
 
-                    combined_chart = alt.layer(
-                        line_chart, line_chart3, arrows
-                    ).properties(width=600, height=400)
-
+                    if isoclinas:
+                        combined_chart = alt.layer(
+                            st.session_state.line_chartrk_3, arrows
+                        ).properties(width=600, height=400)
+                    else:
+                        combined_chart = alt.layer(
+                            st.session_state.line_chartrk_3
+                        ).properties(width=600, height=400)
                 st.altair_chart(combined_chart, use_container_width=True)
 
             except ValueError as e:
@@ -110,4 +122,4 @@ with plot_col:
             except Exception as e:
                 st.error(f"Error inesperado: {e}")
         else:
-            st.write("Por favor, ingresa una ecuación.")
+            st.info("Por favor, ingresa una ecuación.")
